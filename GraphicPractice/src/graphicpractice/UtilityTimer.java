bb9ba3a5f06ddc1171d138a8a399f17591246d81
doliver17 <cokefan158@gmail.com>
2015-01-20 09:34:59 -0800

    Update

---
+++ GraphicPractice/src/graphicpractice/UtilityTimer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphicpractice;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Derik
+ */
+public class UtilityTimer extends JFrame {
+    
+    public UtilityTimer() {
+        initUI();
+    }
+}
